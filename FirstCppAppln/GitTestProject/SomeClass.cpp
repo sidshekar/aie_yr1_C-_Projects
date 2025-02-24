937dfc919e09e00495826b45bb561f0ebceb8458
siddharth.shekar <siddharth.shekar@gmail.com>
2025-02-24 20:12:16 +1100

    Added someclass in github Test project

---
+++ FirstCppAppln/GitTestProject/SomeClass.cpp
@@ -0,0 +1,13 @@
+#include "SomeClass.h"
+
+SomeClass::SomeClass() {
+
+	//added some code here
+
+	someVar = 0;
+}
+
+SomeClass::~SomeClass() {
+
+
+}
---
+++ FirstCppAppln/GitTestProject/SomeClass.h
@@ -0,0 +1,11 @@
+#pragma once
+class SomeClass{
+
+	int someVar;
+
+	SomeClass();
+
+	~SomeClass();
+
+};
+
